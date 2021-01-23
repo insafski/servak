9a2de416a03b6cbb9523a479f24b39a5eb14772b
Insaf Almukhametov <insaf_13@mail.ru>
2021-01-23 08:41:42 +0300

    show logs

--- utils/telegramBot.js
+++ utils/telegramBot.js
@@ -7,6 +7,12 @@ const telegram = (msg) => {
     const message = encodeURI(msg);
     const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat}&parse_mode=html&text=${message}`;
 
-    http.post(url);
+    console.log(url);
+
+    http.post(url, (error, response, body) => {
+        console.error("error:", error);
+        console.log("statusCode:", response && response.statusCode);
+        console.log("body:", body);
+    });
 };
 module.exports = telegram;
